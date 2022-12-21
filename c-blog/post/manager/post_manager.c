cd29904032b1b656cc8840a995187703b3edb159
ciro23 <ciro.sorre23@gmail.com>
2022-12-21 22:29:00 +0100

    beginning of post creation
    asked for user input about post info

---
+++ c-blog/post/manager/post_manager.c
@@ -0,0 +1,27 @@
+#include "post_manager.h"
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+
+void create_post() {
+    Post post = ask_for_post_info();
+}
+
+Post ask_for_post_info() {
+    Post post;
+    
+    printf("What's your username?\n");
+    scanf("%s", post.author);
+    
+    printf("What's the title?\n");
+    scanf("%s", post.title);
+    
+    printf("Write the body\n");
+    scanf("%s", post.body);
+    
+    return post;
+}
+
+long generate_id() {
+    return rand() * pow(10, 6);
+}
---
+++ c-blog/post/manager/post_manager.h
@@ -0,0 +1,15 @@
+#ifndef post_manager_h
+#define post_manager_h
+
+#include <stdio.h>
+#include "../Post.h"
+
+#endif /* post_h */
+
+void create_post(void);
+
+Post ask_for_post_info(void);
+
+void store_post(Post post);
+
+long generate_id(void);
