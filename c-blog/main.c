1022bc8c372f9e754fe47f7e15cfb83dcb1bb71d
ciro23 <ciro.sorre23@gmail.com>
2022-12-23 19:35:14 +0100

    called functions in main
    at the moment the only feature is creating a post, but eventually a menu with different choices will be shown

--- c-blog/main.c
+++ c-blog/main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "post/manager/post_manager.h"
 
 int main(int argc, const char * argv[]) {
+    srand((int) time(NULL));
+
+    create_post();
     return 0;
 }
